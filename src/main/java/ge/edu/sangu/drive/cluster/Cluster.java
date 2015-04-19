15010cc8c68e0f09aa59a3730757e580198cb9ea
Davit Abulashvili <datobula@gmail.com>
2015-04-19 18:59:56 +0400

    added methods

--- src/main/java/ge/edu/sangu/drive/cluster/Cluster.java
+++ src/main/java/ge/edu/sangu/drive/cluster/Cluster.java
@@ -1,7 +1,28 @@
 package ge.edu.sangu.drive.cluster;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author irakli on 4/19/2015.
  */
 public class Cluster {
+
+	private List<Node> nodes = new ArrayList<>();
+
+	public void addNode(Node node) {
+		this.nodes.add(node);
+	}
+
+	public void removeNode(Node node) {
+		this.nodes.remove(node);
+	}
+
+	public void addNodes(List<Node> nodes) {
+		this.nodes.addAll(nodes);
+	}
+
+	public void Connect() {
+
+	}
 }
